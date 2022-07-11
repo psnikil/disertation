000f2720fecaa4d5730979fcb245d38e7ef4b286
psnikil <93334805+psnikil@users.noreply.github.com>
2022-07-11 18:23:27 +0100

    Add files via upload

--- vfnet_taco_1_danu.py
+++ vfnet_taco_1_danu.py
@@ -8,7 +8,7 @@ Created on Mon Jul 11 18:00:53 2022
 _base_ = '/home/s2258567/mmdetection/configs/vfnet/vfnet_x101_64x4d_fpn_mdconv_c3-c5_mstrain_2x_coco.py'
 model = dict(
     pretrained = None,
-    bbox_head=dict(num_classes=6),
+    bbox_head=dict(num_classes=5),
     backbone=dict(init_cfg=dict(type='Pretrained', checkpoint='/home/s2258567/work_dirs/vfnet_taco/latest.pth'))
     )
 
@@ -16,12 +16,12 @@ model = dict(
 dataset_type = 'CocoDataset'
 dataset_type = 'CocoDataset'
 data_root = '/home/s2258567/data/'
-classes = ('_background_','Plastic_Juice_Water_Bottle', 'Paper_Newspaper', 'Cardboard','Paper_Cardboard_Container','Plastic_Shopping_Bag')
+classes = ('Plastic_Juice_Water_Bottle', 'Paper_Newspaper', 'Cardboard','Paper_Cardboard_Container','Plastic_Shopping_Bag')
 data = dict(
    train=dict(
-       img_prefix='/home/s2258567/TACO/data/',
-            classes=classes,
-            ann_file='/home/s2258567/data/train.json'),
+       img_prefix='/home/s2258567/data/train/',
+        classes=classes,
+        ann_file='/home/s2258567/data/train.json'),
     val=dict(
         img_prefix='/home/s2258567/data/val/',
         classes=classes,
